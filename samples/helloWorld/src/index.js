3fd25c13392df20dac9ea95ec337ba76349806c1
Anshul Pandey (anpandey) <anpandey@expedia.com>
2016-01-18 14:10:53 -0800

    expedia test app

--- samples/helloWorld/src/index.js
+++ samples/helloWorld/src/index.js
@@ -21,7 +21,7 @@
 /**
  * App ID for the skill
  */
-var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+var APP_ID = 'amzn1.echo-sdk-ams.app.56e69206-ad61-4056-be43-dd2687d00711'
 
 /**
  * The AlexaSkill prototype and helper functions
@@ -50,8 +50,8 @@ HelloWorld.prototype.eventHandlers.onSessionStarted = function (sessionStartedRe
 
 HelloWorld.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
     console.log("HelloWorld onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
-    var speechOutput = "Welcome to the Alexa Skills Kit, you can say hello";
-    var repromptText = "You can say hello";
+    var speechOutput = "Welcome to the Alexa for Expedia, you can say Get Best Hotel for Seattle";
+    var repromptText = "you can say Get Best Hotel for Seattle";
     response.ask(speechOutput, repromptText);
 };
 
@@ -62,12 +62,30 @@ HelloWorld.prototype.eventHandlers.onSessionEnded = function (sessionEndedReques
 };
 
 HelloWorld.prototype.intentHandlers = {
-    // register custom intent handlers
-    "HelloWorldIntent": function (intent, session, response) {
-        response.tellWithCard("Hello World!", "Greeter", "Hello World!");
-    },
+
     "AMAZON.HelpIntent": function (intent, session, response) {
         response.ask("You can say hello to me!", "You can say hello to me!");
+    },
+    "GetBestHotel" : function (intent, session, response) {
+        var city_slot = intent.slots.city
+        var city = city_slot.value.toString()
+        console.log('city is :', city)
+
+        var laHotelResponse = "Best LA Hotel is SLS Hotel, a Luxury Collection Hotel, Beverly Hills and is located at 465 S La Cienega Blvd. This Beverly Hills hotel, Starwood Luxury Collection, is two blocks from Beverly Center shopping and within 2.5 miles of the Sunset Strip and Rodeo Drive.";
+        var chicagoHotelResponse = "Best Chicago Hotel is Waldorf Astoria Chicago and is located at 11 E. Walton Street. This family-friendly Chicago hotel is located in the historical district, within a 10-minute walk of Newberry Library, Hancock Tower, and Chicago Water Tower. Tribune Tower and Wrigley Building are also within 1 mi (2 km).";
+        var nyHotelResponse = "Best New York hotel is The Carlyle, A Rosewood Hotel and is located at 35 E 76th St. Host to world leaders and entertainment luminaries, this Upper East Side landmark presides over fashionable Madison Avenue, 1 block from Central Park and 5 blocks from the Metropolitan Museum of Art.";
+        var seattleHotelResponse= "Best Seattle Hotel is Fairmont Olympic Hotel and is located at 411 University Street. Built on the original site of the University of Washington, this grand hotel is just a few blocks from the Seattle Art Museum, Pacific Place shops, the Pike Place Market, and the convention center ";
+
+        if (city === "get Los Angeles")
+            response.tellWithCard(laHotelResponse);
+        else if (city === "get Chicago")
+            response.tellWithCard(chicagoHotelResponse);
+        else if (city === "get New York")
+            response.tellWithCard(nyHotelResponse);
+        else if (city === "get Seattle")
+            response.tellWithCard(seattleHotelResponse);
+        else
+            response.tellWithCard("Your city is not yet supported on Alexa for Expedia. Please try Seattle, New York, Chicago or Los Angeles. For next set of cities please stay tuned.");
     }
 };
 
